4210eadbe71ad8f04e1062d40519be97e4770104
Andrei Rassanov <totsamiyandrey@gmail.com>
2020-12-22 13:45:07 +0400

    Fifth review

--- app.js
+++ app.js
@@ -15,6 +15,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT, () => {});
